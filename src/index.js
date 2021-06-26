f45b7bb543f54fc1ce8a643cc5ea61213698960a
jrrohrer <jessie.rohrer@gmail.com>
2021-06-26 12:35:12 -0400

    lab solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskDescription = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+
+  document.getElementById("create-task-form").onsubmit = createTask;
+
+  function createTask(e) {
+    e.preventDefault();
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(newTaskDescription.value));
+    createDeleteBtn(li);
+    list.appendChild(li);
+    newTaskDescription.value = "";
+  }
+
+  function createDeleteBtn(li) {
+    const btn = document.createElement('button');
+    btn.innerText = "X";
+    li.appendChild(btn);
+    btn.addEventListener('click', function(e) {deleteTask(e)});
+  }
+
+  function deleteTask(e) {
+    const selectedTask = e.target.parentElement;
+    list.removeChild(selectedTask);
+  }
 });
+
+
+
